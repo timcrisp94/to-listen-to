7a8ca0dcb36af4525e824165584eddef903c9d5f
Tim Crisp <timothycrisp@gmail.com>
2022-06-27 13:11:00 -0500

    create component folder for todos

--- src/todolist.js
+++ src/todolist.js
@@ -1,29 +1,53 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class TodoList extends Component {
-  // refactor constructor function to modern syntax
-  // hooks instead of ref?
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: []
-    }
-    this.addItem = this.addItem.bind(this)
+// class TodoList extends Component {
+//   // refactor constructor function to modern syntax
+//   // hooks instead of ref?
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       items: []
+//     }
+//     this.addItem = this.addItem.bind(this)
+//   }
+//   render () {
+//     return (
+//       <div className="todoListMain">
+//         <div className="header">
+//           <form onSubmit={this.addItem}>            
+//             {/* <input ref={(a) => this._inputElement = a }
+//             placeholder="enter record"></input> */}
+//             <input placeholder="enter record"></input>
+//             <button type="submit">add</button>
+//           </form>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+
+
+// export default TodoList;
+
+export function TodoList(props) {
+  const [toListenTos, setToListenTos] = useState([])
+
+  function createRecord(record) {
+    const newToListenTos = [...toListenTos, { record }]
+
+    setToListenTos(newToListenTos)
+
+    console.log(newToListenTos)
   }
-  render () {
-    return (
-      <div className="todoListMain">
-        <div className="header">
-          <form onSubmit={this.addItem}>            
-            {/* <input ref={(a) => this._inputElement = a }
-            placeholder="enter record"></input> */}
-            <input placeholder="enter record"></input>
-            <button type="submit">add</button>
-          </form>
-        </div>
-      </div>
-    )
+
+  function deleteRecord(index) {
+    const newToListenTos = [...toListenTos]
+    newToListenTos.splice(index, 1)
+    setToListenTos(newToListenTos)
   }
+
+  return <>
+
+  </>
 }
-
-export default TodoList;
