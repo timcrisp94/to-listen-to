1150c6218652b44578cab3496727e7de233d10e2
Tim Crisp <timothycrisp@gmail.com>
2022-06-27 15:34:40 -0500

    main todo function

---
+++ src/components/TodoMain.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState } from "react"
+import { TodoTitle } from "./TodoTitle";
+import { TodoForm } from "./TodoForm";
+
+export function TodoMain(props) {
+  const [todos, setTodos] = useState([])
+
+  function createTask(task) {
+    const newTodos = [...todos, { task }]
+    setTodos(newTodos)
+
+    console.log(newTodos)
+  }
+
+  function deleteTask(index) {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+
+  return <>
+    <TodoTitle />
+    <TodoForm props={props} todos={[...todos]} createTask={createTask} deleteTask={deleteTask} />    
+  </>
+  
+}
