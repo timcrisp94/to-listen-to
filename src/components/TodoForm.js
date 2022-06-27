605dcf438d7861c3699cf73b4c843ce08bdc3cd8
Tim Crisp <timothycrisp@gmail.com>
2022-06-27 15:26:54 -0500

    todo form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,1 +1,21 @@
 import React, { useState } from "react";
+import { TodoTaskContainer } from "./TodoTaskContainer";
+
+export function TodoForm({props, todos, createTask, deleteTask}) {
+  const [value, setValue] = useState('')
+
+  function handleClick(e) {
+    e.preventDefault()
+    if(!value) { return }
+    createTask(value)
+    setValue('')
+  }
+
+  return(
+    <form onSubmit={handleClick}>
+      <input className="text-input" type="text" value={value} onChange={e => setValue(e.target.value)} />
+      <input className="button" type="submit" value="Add" />
+      <TodoTaskContainer props={props} todos={todos} deleteTask={deleteTask} />
+    </form>
+  )
+}
