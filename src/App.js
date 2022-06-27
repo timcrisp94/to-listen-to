57c72b227230c7883cec7106762f56cca872a0ed
Tim Crisp <timothycrisp@gmail.com>
2022-06-27 12:55:20 -0500

    create constructor function for TodoList, introduce outdated syntax

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './todolist';
 
 function App() {
   return (
     <div className="App">
-      <p>hello</p>
+      <TodoList />
     </div>
   );
 }
--- src/todolist.js
+++ src/todolist.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 
 class TodoList extends Component {
+  // refactor constructor function to modern syntax
+  // hooks instead of ref?
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    }
+    this.addItem = this.addItem.bind(this)
+  }
   render () {
     return (
       <div className="todoListMain">
         <div className="header">
-          <form>
+          <form onSubmit={this.addItem}>            
+            {/* <input ref={(a) => this._inputElement = a }
+            placeholder="enter record"></input> */}
             <input placeholder="enter record"></input>
             <button type="submit">add</button>
           </form>
